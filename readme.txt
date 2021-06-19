206a1d2118fc92f98043d1e70352f2fc2884e02d
Katerina <kvitsaxaki@csd.uoc.gr>
2021-06-19 22:11:12 +0300

    final commit

---
+++ readme.txt
@@ -0,0 +1,11 @@
+(1) Our ontology from stage 2 is consistent. The method we constructed can validate it.
+
+For question 6 to run, you have to install py4j package with the command: 
+pip3 install py4j
+
+You also need to download the robot.jar file from the following link, and place it in the same folder as the other files(it is needed in stage3.py line 49; you can change the path specified there if you want):
+https://github.com/ontodev/robot/releases/tag/v1.8.1
+
+csd3881
+csdp1186
+csdp1151
--- stage3.py
+++ stage3.py
@@ -1,4 +1,5 @@
 from owlready2 import *
+from py4j.java_gateway import launch_gateway, JavaGateway
 
 #(1) if an ontology is incosistent, running the reasoner
 #will raise the OwlReadyInconsistentOntologyError exception
@@ -13,9 +14,15 @@ def is_consistent (ontology):
 
 
 #(2) 
-def is_of_types(ontology, individual):
-	with ontology: sync_reasoner(infer_property_values = True)
-	return individual.is_a
+def is_of_types(individual):
+
+	concepts = []
+	for c in individual.is_a:
+		ancestors = c.ancestors()
+		for a in ancestors:
+			if a not in concepts:
+				concepts.append(a)
+	return concepts
 
 
 
@@ -28,31 +35,64 @@ def get_all_concept_instances(concept):
 def get_all_role_instances(role):
 	return list(role.get_relations())
 
+
 #(5)
 def is_subclass_of(ontology, C, D):
+
 	subclasses_of_D = ontology.search(subclass_of = D)
-	
-	#BUG!!!!!!!!!!!!!!!!!!
-	# print(subclasses_of_D)
-	
-	if C in subclasses_of_D:
-		return True
-	else:
-		return False
+	return (subclasses_of_D.count(C)>0)
+
 
 #(6)
-def get_S_module(ontology, S):
-	pass
+def get_S_module(onto_path, signature_path):
+	
+	launch_gateway(jarpath='./robot.jar',
+               classpath='org.obolibrary.robot.PythonOperation',
+               port=25333,
+               die_on_exit=True)
+
+	gateway = JavaGateway()
+
+	io_helper = gateway.jvm.org.obolibrary.robot.IOHelper()
+	extractor = gateway.jvm.org.obolibrary.robot.ExtractOperation()
+
+
+	ModuleType = gateway.jvm.uk.ac.manchester.cs.owlapi.modularity.ModuleType
+	starModuleType = ModuleType.STAR
+
+
+	io_helper.saveOntology(
+		extractor.extract(
+			io_helper.loadOntology(onto_path),
+			io_helper.parseTerms(signature_path),
+			io_helper.createIRI("http://example.com"),
+			starModuleType
+		),
+		'result.owl'
+		)
 
 
 
 #------------------testing----------------------
 
-onto = get_ontology("./output.owl").load()
+onto = get_ontology("./ontology.owl").load()
 
-# print(is_consistent(onto))
-# print(is_of_types(onto, onto.uSt0))
-# print(get_all_concept_instances(onto.PostgraduateStudies))
-# print(get_all_role_instances(onto.prerequisites))
+#1
+print(is_consistent(onto))
+
+#2
+print(is_of_types(onto.gradC0))
+print(is_of_types(onto.uSt0))
+
+#3
+print(get_all_concept_instances(onto.CSDEmployee))
+
+#4
+print(get_all_role_instances(onto.advisor))
+
+#5
 print(is_subclass_of(onto, onto.PhDStudent, onto.Person))
+print(is_subclass_of(onto, onto.PhDStudent, onto.Course))
 
+#6
+get_S_module("./ontology.owl", "./signature.txt")
